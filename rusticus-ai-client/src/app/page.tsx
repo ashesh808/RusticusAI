56f6bdfcdce6ef57ffa4a3739e356082ebcda7ce
Guy Buhendwa <SS6006WQ@go.minnstate.edu>
2024-03-16 09:52:56 -0500

    Homepage

--- rusticus-ai-client/src/app/page.tsx
+++ rusticus-ai-client/src/app/page.tsx
@@ -2,111 +2,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24 relative bg-gray-400">
+      {/* Courses */}
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-bold text-lg lg:flex">
+        <p className="absolute left-10 top-5 flex justify-center items-center border-white-600">
+          Courses
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      {/* Course Items */}
+      <div className="absolute left-10 top-20 flex flex-col space-y-4 items-start justify-end rounded-lg">
+        {/* Rectangle 1 */}
+        <div className="course-item h-16 md:h-24 w-36 md:w-64 bg-blue-600 text-white flex justify-center items-center hover:bg-blue-600 rounded-lg p-2 md:p-4 cursor-pointer font-bold">
+          Course 1
+        </div>
+        {/* Rectangle 2 */}
+        <div className="course-item h-16 md:h-24 w-36 md:w-64 bg-blue-600 text-white flex justify-center items-center hover:bg-blue-600 rounded-lg p-2 md:p-4 cursor-pointer font-bold">
+          Course 2
+        </div>
+        {/* Rectangle 3 */}
+        <div className="course-item h-16 md:h-24 w-36 md:w-64 bg-blue-600 text-white flex justify-center items-center hover:bg-blue-600 rounded-lg p-2 md:p-4 cursor-pointer font-bold">
+          Course 3
+        </div>
+        {/* Rectangle 4 */}
+        <div className="course-item h-16 md:h-24 w-36 md:w-64 bg-blue-600 text-white flex justify-center items-center hover:bg-blue-600 rounded-lg p-2 md:p-4 cursor-pointer font-bold">
+          Course 4
+        </div>
+        {/* Add a course */}
+        <div className="course-item h-16 md:h-12 w-36 md:w-64 bg-green-600 text-white flex justify-center items-center hover:bg-green-600 p-2 md:p-4 cursor-pointer rounded-lg font-bold">
+          Add a course
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Course Summary */}
+      <div className="course-summary absolute right-5 top-5 md:right-40 ">
+        <p className="font-bold text-lg text-white ">Course Summary</p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Summary Box */}
+      <div className="summary-box absolute right-10 top-20 md:right-20 md:w-96 h-96 flex flex-col items-center justify-top bg-gray-100 rounded-lg p-4 font-bold">
+        {/* Additional summary content can be added here */}
+        <p style={{ color: "black" }}>Summary content will display here</p>
       </div>
     </main>
   );
